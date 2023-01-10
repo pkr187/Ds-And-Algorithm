116d1f2af54f593d1a3cd6f32c48339df0e4ec5f
pkr187 <pankajroman187@gmail.com>
2023-01-11 00:01:08 +0530

    [pankaj] Add:BinarySearchWord

---
+++ DsAndAlgorithm/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DsAndAlgorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Select Programs \n2.Binary search");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 2:
+                        BinarySearch binarysearch = new BinarySearch();
+                        binarysearch.Search(@"C:\DsAlgorithm\DsAndAlgorithm\Word.txt");
+                        break;
+                }
+            }
+        }
+    }
+}
